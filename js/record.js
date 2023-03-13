6940550b9a7c785b9884cd53d62a60170ea5b000
whgustj0831 <122263057+whgustj0831@users.noreply.github.com>
2023-03-13 17:22:16 +0900

    Add files via upload

---
+++ js/record.js
@@ -0,0 +1,30 @@
+(function($){
+
+    if (localStorage.gdata) {
+        gdata = JSON.parse(localStorage.gdata)
+        if(gdata){
+            id = gdata[gdata.length-1].id
+        }
+        usedata(gdata)
+    }
+    
+    $('#star').on('click', '.addS',function(){
+        let aname = $(this).attr('data-statn')
+        let obj = { id:id, text:aname }
+        gdata.push(obj)
+        localStorage.gdata = JSON.stringify(gdata)
+        usedata(gdata)
+        console.log('gdata',gdata)
+    })
+
+    function usedata(rdata){
+        $('#record div').remove()
+        let list = `<div class="cord">`
+        rdata.map(function(value){
+            list += `<a href="./trainInfo?statn_nm=${value.text}">${value.text}</a>`
+        })
+        list += `</div>`
+        $('#record .re').before(list)
+    }
+
+})(jQuery)
